b2ed22cdc8849deac11d9d13d929a859c48f1895
Yaulendil <davarice@protonmail.com>
2021-11-21 03:19:51 -0500

    Overhaul options and setup procedure

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,6 @@
 use directories::ProjectDirs;
 use std::{
-    fmt::{Display, Formatter},
-    fs::{create_dir, File},
+    fs::{create_dir, File, rename},
     io::{Error as ErrorIo, Read, Write},
     path::{Path, PathBuf},
     time::Duration,
@@ -41,6 +40,13 @@ fn find_path(create_missing: bool) -> Option<PathBuf> {
 }
 
 
+fn get_backup(path: &Path) -> Option<PathBuf> {
+    Some(path.with_file_name(
+        format!(".bkp.{}", path.file_name()?.to_string_lossy())
+    ))
+}
+
+
 fn lower(vec: &mut Vec<String>) {
     for name in vec.iter_mut() {
         name.make_ascii_lowercase();
@@ -48,47 +54,27 @@ fn lower(vec: &mut Vec<String>) {
 }
 
 
-#[derive(Debug)]
-pub enum ConfigError {
-    Io(ErrorIo),
+pub enum ConfigFind {
+    DoesNotExist(PathBuf),
+    Exists(PathBuf, ConfigOpen),
     NoPath,
-    ParseError(ErrorToml),
 }
 
-impl ConfigError {
-    pub fn status(&self) -> i32 {
+impl ConfigFind {
+    pub fn path(&self) -> Option<&PathBuf> {
         match self {
-            ConfigError::Io(err) => err.raw_os_error().unwrap_or(1),
-            _ => 1,
+            Self::NoPath => None,
+            Self::Exists(path, _)
+            | Self::DoesNotExist(path) => Some(path),
         }
     }
 }
 
 
-impl From<ErrorIo> for ConfigError {
-    fn from(e: ErrorIo) -> Self { Self::Io(e) }
-}
-
-
-impl From<ErrorToml> for ConfigError {
-    fn from(e: ErrorToml) -> Self { Self::ParseError(e) }
-}
-
-
-impl Display for ConfigError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ConfigError::Io(err) => {
-                write!(f, "Failed to access Configuration file: {}", err)
-            }
-            ConfigError::NoPath => {
-                write!(f, "Failed to find Configuration filepath.")
-            }
-            ConfigError::ParseError(err) => {
-                write!(f, "Failed to parse Configuration: {}", err)
-            }
-        }
-    }
+pub enum ConfigOpen {
+    FileInaccessible(ErrorIo),
+    FileInvalid(ErrorToml),
+    FileValid(Config),
 }
 
 
@@ -126,6 +112,57 @@ pub struct Config {
     auction: ConfigAuction,
 
     channel: Option<Map<String, Value>>,
+}
+
+
+impl Config {
+    pub fn create(path: &Path, create_parent: bool) -> Result<(), ErrorIo> {
+        if let Some(parent) = path.parent() {
+            if create_parent && !parent.exists() {
+                create_dir(parent)?;
+            }
+        }
+
+        if path.exists() {
+            if let Some(backup) = get_backup(path) {
+                rename(path, backup).ok();
+            }
+        }
+
+        File::create(path)?.write_all(CONFIG_DEFAULT.as_bytes())
+    }
+
+    pub fn find(path_opt: Option<PathBuf>) -> ConfigFind {
+        use ConfigFind::*;
+
+        match path_opt.or_else(|| find_path(false)) {
+            None => NoPath,
+            Some(path) if !path.exists() => DoesNotExist(path),
+            Some(path) => {
+                let open = Self::open(&path);
+                Exists(path, open)
+            }
+        }
+    }
+
+    pub fn open(path: &Path) -> ConfigOpen {
+        use ConfigOpen::*;
+
+        let mut data = String::with_capacity(CONFIG_SIZE);
+        if let Err(e) = File::open(&path).and_then(|mut file: File| {
+            file.read_to_string(&mut data)
+        }) {
+            return FileInaccessible(e);
+        }
+
+        match toml::from_str::<Config>(&data) {
+            Err(e) => FileInvalid(e),
+            Ok(mut config) => {
+                config.lower();
+                FileValid(config)
+            }
+        }
+    }
 }
 
 
@@ -193,87 +230,5 @@ impl Config {
     pub fn lower(&mut self) {
         lower(&mut self.admin.admins);
         lower(&mut self.admin.blacklist);
-    }
-}
-
-
-impl Config {
-    pub fn ensure(path_opt: Option<PathBuf>, force: bool) -> i32 {
-        match path_opt.or_else(|| find_path(true)) {
-            Some(path) if force || !path.exists() => {
-                println!("Creating new Config file: {}", path.display());
-
-                match File::create(&path) {
-                    Ok(mut f) => match f.write_all(CONFIG_DEFAULT.as_bytes()) {
-                        Ok(..) => {
-                            println!("Default Config written successfully.");
-                            0
-                        }
-                        Err(e) => {
-                            println!("Failed to write default Config: {}", e);
-                            1
-                        }
-                    }
-                    Err(e) => {
-                        println!("Failed to create file: {}", e);
-                        1
-                    }
-                }
-            }
-            Some(path) => {
-                println!("Found existing Config file: {}", path.display());
-                0
-            }
-            None => {
-                println!("Failed to find a path for the Config file.");
-                1
-            }
-        }
-    }
-
-    pub fn find(path_opt: Option<PathBuf>) -> i32 {
-        let opt: Option<PathBuf> = path_opt.or_else(|| find_path(false));
-
-        match &opt {
-            Some(path) if path.exists() => println!(
-                "Found existing Config file: {}",
-                path.display(),
-            ),
-            Some(path) => println!(
-                "Config file does not exist: {}",
-                path.display(),
-            ),
-            None => println!("Failed to find a path for the Config file."),
-        }
-
-        opt.is_none() as i32
-    }
-
-    pub fn from_path(path: impl AsRef<Path>) -> Result<Self, ConfigError> {
-        let path = path.as_ref();
-
-        if path.exists() {
-            info!("Using existing Config file: {}", path.display());
-        } else {
-            let mut file = File::create(&path)?;
-            info!("New Config file created: {}", path.display());
-
-            file.write_all(CONFIG_DEFAULT.as_bytes())?;
-        }
-
-        let mut data = String::with_capacity(CONFIG_SIZE);
-        { File::open(&path)?.read_to_string(&mut data)? };
-
-        let mut new: Config = toml::from_str(&data)?;
-        new.lower();
-
-        Ok(new)
-    }
-
-    pub fn setup() -> Result<Self, ConfigError> {
-        match find_path(true) {
-            Some(path) => Self::from_path(path),
-            None => Err(ConfigError::NoPath),
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,128 @@
 use argh::{from_env, FromArgs};
-use k2o::*;
+use k2o::{*, config::{ConfigFind::*, ConfigOpen::{*, self}}};
 use std::{path::PathBuf, process::exit, thread::Builder};
 
 
 /// Run an Auction bot in Twitch chat via the IRC Bridge.
 #[derive(FromArgs)]
-#[argh(note="\
-A default Configuration file is written upon first execution. The generated
-file must then be modified to supply valid Twitch authentication data before
-a successful connection can be made.")]
+#[argh(note = "\
+A default Configuration file is written upon first execution, or when run
+with `--mkconf`. The generated file must then be modified to supply valid
+Twitch authentication data before any successful connections can be made.")]
 struct Command {
     /// channels to be joined
     #[argh(positional, arg_name = "CHANNEL")]
     channels: Vec<String>,
 
     /// specify a path for the Config file
-    #[argh(option, long = "config", arg_name = "PATH")]
+    #[argh(option, long = "cfg", arg_name = "PATH")]
     cfg_path: Option<PathBuf>,
 
-    /// check for the existence of a Config file and exit
-    #[argh(switch)]
-    lsconf: bool,
-
-    /// ensure the existence of a Config file and exit
-    #[argh(switch)]
-    mkconf: bool,
-
     /// check the Config file for validity and exit
-    #[argh(switch)]
-    okconf: bool,
+    #[argh(switch, long = "cfg-check")]
+    check_cfg: bool,
 
-    /// overwrite any existing Config file with the default values
-    #[argh(switch)]
-    reinit: bool,
+    /// create a new default Config file and exit
+    #[argh(switch, long = "mkconf")]
+    make_cfg: bool,
 }
 
 
 fn main() {
     if let Err(..) = ctrlc::set_handler(|| {
-        eprintln!("SIGINT");
+        eprintln!("Interrupted");
         stop();
     }) {
         fatal!("Failed to set Interrupt Handler.");
         exit(1);
     }
 
-    let Command {
-        cfg_path, channels,
-        lsconf, mkconf, okconf, reinit,
-    } = from_env();
+    let Command { cfg_path, channels, check_cfg, make_cfg } = from_env();
 
-    if okconf {
-        use config::ConfigError::ParseError;
-
-        match match cfg_path {
-            Some(path) => Config::from_path(path),
-            None => Config::setup(),
-        } {
-            Ok(..) => println!("Config file is valid."),
-            Err(ParseError(e)) => println!("Config file is NOT valid: {}", e),
-            Err(e) => {
-                err!("Failed to check Config file: {}", e);
-                exit(1);
+    if check_cfg {
+        exit(match Config::find(cfg_path) {
+            DoesNotExist(path) => {
+                println!("Config file at {} does not exist.", path.display());
+                1
             }
+            Exists(path, FileInaccessible(e)) => {
+                println!("Config file at {} inaccessible: {}", path.display(), e);
+                1
+            }
+            Exists(path, FileInvalid(e)) => {
+                println!("Config file at {} invalid: {}", path.display(), e);
+                1
+            }
+            Exists(path, FileValid(..)) => {
+                println!("Valid Config file found: {}", path.display());
+                0
+            }
+            NoPath => {
+                println!("Cannot find Config filepath.");
+                1
+            }
+        });
+    } else if make_cfg {
+        exit(match Config::find(cfg_path).path() {
+            Some(path) => match Config::create(&path, true) {
+                Ok(..) => {
+                    println!("Default Config file created: {}", path.display());
+                    0
+                }
+                Err(e) => {
+                    println!(
+                        "Failed to write {} as default Config file: {}",
+                        path.display(), e,
+                    );
+                    1
+                }
+            }
+            None => {
+                println!("Cannot find Config filepath.");
+                1
+            }
+        })
+    } else {
+        if channels.is_empty() {
+            err!("Provide at least one Channel to join.");
+            exit(1);
         }
 
-    } else if lsconf {
-        exit(Config::find(cfg_path));
+        let (path, open): (PathBuf, ConfigOpen) = match Config::find(cfg_path) {
+            Exists(path, open) => {
+                info!("Using existing Config file: {}", path.display());
+                (path, open)
+            }
+            DoesNotExist(path) => match Config::create(&path, true) {
+                Ok(..) => {
+                    info!("Default Config file created: {}", path.display());
+                    let open = Config::open(&path);
+                    (path, open)
+                }
+                Err(e) => {
+                    err!(
+                        "Failed to write {} as default Config file: {}",
+                        path.display(), e,
+                    );
+                    exit(1);
+                }
+            },
+            NoPath => {
+                err!("Cannot find Config file path.");
+                exit(1);
+            }
+        };
 
-    } else if mkconf {
-        exit(Config::ensure(cfg_path, reinit));
-
-    } else if channels.is_empty() {
-        err!("Provide at least one Channel to join.");
-        exit(1);
-
-    } else {
-        match match cfg_path {
-            Some(path) => Config::from_path(path),
-            None => Config::setup(),
-        } {
-            Ok(config) => {
+        match open {
+            FileInaccessible(e) => {
+                err!("Config file at {} inaccessible: {}", path.display(), e);
+                exit(1);
+            }
+            FileInvalid(e) => {
+                err!("Config file at {} invalid: {}", path.display(), e);
+                exit(1);
+            }
+            FileValid(config) => {
                 let mut threads = Vec::with_capacity(channels.len());
 
                 for channel in channels.into_iter() {
@@ -91,7 +133,7 @@ fn main() {
                         .name(format!("#{}", channel))
                         .spawn(move || run_bot(channel, &cfg))
                     {
-                        Err(error) => err!("Failed to spawn thread: {}", error),
+                        Err(error) => warn!("Failed to spawn thread: {}", error),
                         Ok(thread) => threads.push(thread),
                     }
                 }
@@ -101,10 +143,6 @@ fn main() {
                         err!("{:?}", err);
                     }
                 }
-            }
-            Err(error) => {
-                err!("{}", &error);
-                exit(error.status());
             }
         }
     }
