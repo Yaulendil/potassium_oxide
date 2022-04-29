31e2ca4aaddab581702344059e02e380d4f37127
Yaulendil <davarice@protonmail.com>
2022-04-29 15:33:01 -0400

    Break `main()` into sane pieces

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use std::{path::PathBuf, process::exit, thread::Builder};
+mod run;
+
+use std::{path::PathBuf, process::exit};
 use argh::{from_env, FromArgs};
-use k2o::{
-    {err, fatal, info, warn},
-    config::{Config, ConfigFind, ConfigOpen},
-    run_bot,
-    stop,
-};
 
 
 /// Run an Auction bot in Twitch chat via the IRC Bridge.
@@ -36,122 +32,21 @@ struct Command {
 fn main() {
     if let Err(e) = ctrlc::set_handler(|| {
         eprintln!("Interrupted");
-        stop();
+        k2o::stop();
     }) {
-        fatal!("Failed to set Interrupt Handler: {}", e);
+        k2o::fatal!("Failed to set Interrupt Handler: {}", e);
         exit(1);
     }
 
     let Command { cfg_path, channels, check_cfg, make_cfg } = from_env();
 
     if check_cfg {
-        exit(match Config::find(cfg_path) {
-            ConfigFind::DoesNotExist(path) => {
-                println!("Config file at {} does not exist.", path.display());
-                1
-            }
-            ConfigFind::Exists(path, ConfigOpen::FileInaccessible(e)) => {
-                println!("Config file at {} inaccessible: {}", path.display(), e);
-                1
-            }
-            ConfigFind::Exists(path, ConfigOpen::FileInvalid(e)) => {
-                println!("Config file at {} invalid: {}", path.display(), e);
-                1
-            }
-            ConfigFind::Exists(path, ConfigOpen::FileValid(..)) => {
-                println!("Valid Config file found: {}", path.display());
-                0
-            }
-            ConfigFind::NoPath => {
-                println!("Cannot find Config filepath.");
-                1
-            }
-        });
+        run::cfg_check(cfg_path);
     } else if make_cfg {
-        exit(match Config::find(cfg_path).path() {
-            Some(path) => match Config::create(&path, true) {
-                Ok(..) => {
-                    println!("Default Config file created: {}", path.display());
-                    0
-                }
-                Err(e) => {
-                    println!(
-                        "Failed to write {} as default Config file: {}",
-                        path.display(), e,
-                    );
-                    1
-                }
-            }
-            None => {
-                println!("Cannot find Config filepath.");
-                1
-            }
-        })
+        run::cfg_make(cfg_path);
     } else {
-        if channels.is_empty() {
-            err!("Provide at least one Channel to join.");
-            exit(1);
-        }
-
-        info!("Starting Potassium Oxide v{}", env!("CARGO_PKG_VERSION"));
-
-        let (path, open): (PathBuf, ConfigOpen) = match Config::find(cfg_path) {
-            ConfigFind::Exists(path, open) => {
-                info!("Using existing Config file: {}", path.display());
-                (path, open)
-            }
-            ConfigFind::DoesNotExist(path) => match Config::create(&path, true) {
-                Ok(..) => {
-                    info!("Default Config file created: {}", path.display());
-                    let open = Config::open(&path);
-                    (path, open)
-                }
-                Err(e) => {
-                    err!(
-                        "Failed to write {} as default Config file: {}",
-                        path.display(), e,
-                    );
-                    exit(1);
-                }
-            }
-            ConfigFind::NoPath => {
-                err!("Cannot find Config file path.");
-                exit(1);
-            }
-        };
-
-        match open {
-            ConfigOpen::FileInaccessible(e) => {
-                err!("Config file at {} inaccessible: {}", path.display(), e);
-                exit(1);
-            }
-            ConfigOpen::FileInvalid(e) => {
-                err!("Config file at {} invalid: {}", path.display(), e);
-                exit(1);
-            }
-            ConfigOpen::FileValid(config) => {
-                let mut threads = Vec::with_capacity(channels.len());
-                let config = config.with_path(path);
-
-                for channel in channels.into_iter() {
-                    info!("Joining #{}...", &channel);
-                    let cfg = config.clone();
-
-                    match Builder::new()
-                        .name(format!("#{}", channel))
-                        .spawn(move || run_bot(channel, cfg))
-                    {
-                        Err(error) => warn!("Failed to spawn thread: {}", error),
-                        Ok(thread) => threads.push(thread),
-                    }
-                }
-
-                for thread in threads {
-                    if let Err(err) = thread.join() {
-                        err!("{:?}", err);
-                    }
-                }
-            }
-        }
+        run::bot(cfg_path, channels);
     }
+
+    // unreachable!()
 }
---
+++ src/run.rs
@@ -0,0 +1,132 @@
+//! Runner functions to be called from `main.rs`. These were previously inline
+//!     in `main()`, and are not part of `lib.rs`.
+
+use std::{path::PathBuf, process::exit, thread::Builder};
+use k2o::{
+    {err, info, warn},
+    config::{Config, ConfigFind, ConfigOpen},
+    run_bot,
+};
+
+
+pub fn cfg_check(cfg_path: Option<PathBuf>) -> ! {
+    use ConfigFind::*;
+    use ConfigOpen::*;
+
+    match Config::find(cfg_path) {
+        DoesNotExist(path) => {
+            println!("Config file at {} does not exist.", path.display());
+            exit(1);
+        }
+        Exists(path, FileInaccessible(e)) => {
+            println!("Config file at {} inaccessible: {}", path.display(), e);
+            exit(1);
+        }
+        Exists(path, FileInvalid(e)) => {
+            println!("Config file at {} invalid: {}", path.display(), e);
+            exit(1);
+        }
+        Exists(path, FileValid(..)) => {
+            println!("Valid Config file found: {}", path.display());
+            exit(0);
+        }
+        NoPath => {
+            println!("Cannot find Config filepath.");
+            exit(1);
+        }
+    }
+}
+
+
+pub fn cfg_make(cfg_path: Option<PathBuf>) -> ! {
+    match Config::find(cfg_path).path() {
+        Some(path) => match Config::create(&path, true) {
+            Ok(..) => {
+                println!("Default Config file created: {}", path.display());
+                exit(0);
+            }
+            Err(e) => {
+                println!(
+                    "Failed to write {} as default Config file: {}",
+                    path.display(), e,
+                );
+                exit(1);
+            }
+        }
+        None => {
+            println!("Cannot find Config filepath.");
+            exit(1);
+        }
+    }
+}
+
+
+pub fn bot(cfg_path: Option<PathBuf>, channels: Vec<String>) -> ! {
+    if channels.is_empty() {
+        err!("Provide at least one Channel to join.");
+        exit(1);
+    }
+
+    info!("Starting Potassium Oxide v{}", env!("CARGO_PKG_VERSION"));
+
+    let (path, open): (PathBuf, ConfigOpen) = match Config::find(cfg_path) {
+        ConfigFind::Exists(path, open) => {
+            info!("Using existing Config file: {}", path.display());
+            (path, open)
+        }
+        ConfigFind::DoesNotExist(path) => match Config::create(&path, true) {
+            Ok(..) => {
+                info!("Default Config file created: {}", path.display());
+                let open = Config::open(&path);
+                (path, open)
+            }
+            Err(e) => {
+                err!(
+                    "Failed to write {} as default Config file: {}",
+                    path.display(), e,
+                );
+                exit(1);
+            }
+        }
+        ConfigFind::NoPath => {
+            err!("Cannot find Config file path.");
+            exit(1);
+        }
+    };
+
+    match open {
+        ConfigOpen::FileInaccessible(e) => {
+            err!("Config file at {} inaccessible: {}", path.display(), e);
+            exit(1);
+        }
+        ConfigOpen::FileInvalid(e) => {
+            err!("Config file at {} invalid: {}", path.display(), e);
+            exit(1);
+        }
+        ConfigOpen::FileValid(config) => {
+            let mut threads = Vec::with_capacity(channels.len());
+            let config = config.with_path(path);
+
+            for channel in channels.into_iter() {
+                info!("Joining #{}...", &channel);
+                let cfg = config.clone();
+
+                match Builder::new()
+                    .name(format!("#{}", channel))
+                    .spawn(move || run_bot(channel, cfg))
+                {
+                    Err(error) => warn!("Failed to spawn thread: {}", error),
+                    Ok(thread) => threads.push(thread),
+                }
+            }
+
+            for thread in threads {
+                if let Err(err) = thread.join() {
+                    err!("{:?}", err);
+                }
+            }
+
+            exit(0);
+        }
+    }
+}
